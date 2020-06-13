2b79d093ef35356000fdd6ed6c003d1c9b4d4030
Ellpm <ellpm@ya.ru>
2020-06-13 19:11:02 +0300

    fix menu

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink, Link, Switch, Route, Redirect } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -13,8 +14,8 @@ import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import routes from "./routes";
+
 const useStyles = makeStyles(
   (theme) => ({
     root: {
@@ -39,54 +40,48 @@ const useStyles = makeStyles(
 
 export default function ButtonAppBar() {
   const classes = useStyles();
- const [state, setState] = React.useState({
-   left: false,
- });
+  const [state, setState] = React.useState({
+    left: false,
+  });
 
- const toggleDrawer = (anchor, open) => (event) => {
-   if (
-     event &&
-     event.type === "keydown" &&
-     (event.key === "Tab" || event.key === "Shift")
-   ) {
-     return;
-   }
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-   setState({ ...state, [anchor]: open });
- };
+    setState({ ...state, [anchor]: open });
+  };
 
- const list = (anchor) => (
-   <div
-     className={clsx(classes.list, {
-       [classes.fullList]: anchor === "top" || anchor === "bottom",
-     })}
-     role="presentation"
-     onClick={toggleDrawer(anchor, false)}
-     onKeyDown={toggleDrawer(anchor, false)}
-   >
-     <List>
-       {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-         <ListItem button key={text}>
-           <ListItemIcon>
-             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-           </ListItemIcon>
-           <ListItemText primary={text} />
-         </ListItem>
-       ))}
-     </List>
-     <Divider />
-     <List>
-       {["All mail", "Trash", "Spam"].map((text, index) => (
-         <ListItem button key={text}>
-           <ListItemIcon>
-             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-           </ListItemIcon>
-           <ListItemText primary={text} />
-         </ListItem>
-       ))}
-     </List>
-   </div>
- );
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {routes.map((punkt) => (
+          <ListItem
+            button
+            key={punkt.menu}
+            button
+            component={Link}
+            to={punkt.route}
+          >
+            <ListItemIcon>{punkt.icon}</ListItemIcon>
+            <ListItemText primary={punkt.menu} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
 
   return (
     <>
@@ -110,49 +105,23 @@ export default function ButtonAppBar() {
         </AppBar>
       </div>
       <div>
-            <SwipeableDrawer
-              anchor={"left"}
-              open={state['left']}
-              onClose={toggleDrawer('left', false)}
-              onOpen={toggleDrawer('left', true)}
-            >
-              {list('left')}
-            </SwipeableDrawer>
+        <SwipeableDrawer
+          anchor={"left"}
+          open={state["left"]}
+          onClose={toggleDrawer("left", false)}
+          onOpen={toggleDrawer("left", true)}
+        >
+          {list("left")}
+        </SwipeableDrawer>
       </div>
     </>
   );
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import React, { Component } from 'react'
 // import { Link } from "react-router-dom";
 
-
-
-
-
 // export default function Header({ ...rest }) {
-
-
 
 //     return (
 //       <>
@@ -161,8 +130,7 @@ export default function ButtonAppBar() {
 //           <br></br>
 //           <Link to="/base">Base</Link>
 //         </div>
-        
-       
+
 //       </>
 //     );
 // }
---
+++ frontend/src/Components/routes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
+import ShowChartIcon from "@material-ui/icons/ShowChart";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
+import Home from './Home'
+import Base from './Base/Base'
+import Statistic from './Statistic/Statistic'
+import FAQ from './Faq/FAQ'
+import Contact from './Contacts/Contacts'
+
+
+
+const routes = [
+  { menu: "Главная", icon: <AccountBalanceIcon />,
+route: '/', component: Home },
+  { menu: "База данных", icon: <AssignmentIndIcon />,
+route: '/base', component: Base },
+  { menu: "Статистика", icon: <ShowChartIcon />,
+route: '/statistic', component: Statistic },
+  { menu: "FAQ", icon: <QuestionAnswerIcon />,
+route: '/faq', component: FAQ },
+  { menu: "Контакты", icon: <ContactMailIcon />,
+route: '/contacts', component: Contact },
+];
+
+
+export default routes;
